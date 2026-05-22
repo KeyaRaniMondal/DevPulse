62e124e03117253944942aacf2eea06f70aa18b4
KeyaRaniMondal <keyamondal549@gmail.com>
2026-05-22 13:56:00 +0600

    connected database with neondb

---
+++ src/config/schema.sql
@@ -0,0 +1,22 @@
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    role VARCHAR(20) NOT NULL DEFAULT 'contributor',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE issues (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(150) NOT NULL,
+    description TEXT NOT NULL,
+    type VARCHAR(30) NOT NULL,
+    status VARCHAR(30) DEFAULT 'open',
+    reporter_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
