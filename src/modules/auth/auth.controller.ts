315f31de21e59ac6576387bb004cdd242729a6a8
KeyaRaniMondal <keyamondal549@gmail.com>
2026-05-23 02:12:55 +0600

    created auth controller to handle signup & login

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,37 @@
+import type { Request, Response } from "express"
+import { StatusCodes } from "http-status-codes"
+import { loginUser, signupUser } from "./auth.service"
+import { loginSchema, signupSchema } from "./auth.validation"
+
+export const signupController = async (req: Request, res: Response) => {
+    try {
+        const parsed = signupSchema.parse(req.body)
+        const user = await signupUser(parsed)
+        res.status(StatusCodes.CREATED).json({
+            success: true,
+            message: "User created successfully",
+            data: user
+        })
+    } catch (error: any) {
+        res.status(StatusCodes.BAD_REQUEST).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+export const loginController = async (req: Request, res: Response) => {
+    try {
+        const parsed = loginSchema.parse(req.body)
+        const user = await loginUser(parsed.email, parsed.password)
+        res.status(StatusCodes.OK).json({
+            success: true,
+            message: "Logged In successfully",
+            data: user
+        })
+    } catch (error: any) {
+        res.status(StatusCodes.UNAUTHORIZED).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
